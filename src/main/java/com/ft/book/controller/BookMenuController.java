20d5d4bc311f73569ba8477f77dd2546b06a69a6
taofut <330593057@qq.com>
2019-10-13 17:57:43 +0800

    first commit

---
+++ src/main/java/com/ft/book/controller/BookMenuController.java
@@ -0,0 +1,16 @@
+package com.ft.book.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 创建人：taofut
+ * 创建时间：2019-10-13 17:45
+ * 描述：
+ */
+@RestController
+@RequestMapping("/menu")
+public class BookMenuController {
+
+
+}
