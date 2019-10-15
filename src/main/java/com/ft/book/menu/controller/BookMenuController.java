516c0194d9c4a3ac9fe6af5ced648ebf1b0bcdc8
taofut <330593057@qq.com>
2019-10-15 20:43:18 +0800

    tree menu

--- src/main/java/com/ft/book/menu/controller/BookMenuController.java
+++ src/main/java/com/ft/book/menu/controller/BookMenuController.java
@@ -1,10 +1,12 @@
 package com.ft.book.menu.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.ft.book.menu.bean.Menu;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,6 +36,38 @@ public class BookMenuController {
         menu0.setChildren(menu00);
         menuList.add(menu0);
         return menuList;
+    }
+
+    @RequestMapping("/getMenuTree")
+    public String getMenuTree(HttpServletRequest request){
+
+        JSONObject menuNode9 = new JSONObject();
+        JSONObject menuNode10 = new JSONObject();
+
+        menuNode10.put("nodeId", "ZTBGL");
+        menuNode10.put("name", "主题表管理");
+        menuNode10.put("url", request.getContextPath() + "/tzzk/html/ztb/ztbgl.html");
+        menuNode10.put("nodeType", "LEAF");
+        menuNode9.put("nodeId", "ZBGL");
+        menuNode9.put("name", "指标管理");
+        menuNode9.put("url", request.getContextPath() + "/tzzk/html/zbgl/zbgl.html");
+        menuNode9.put("nodeType", "LEAF");
+
+        JSONArray menuNodeListJspzgl = new JSONArray();
+        menuNodeListJspzgl.add(menuNode10);
+        menuNodeListJspzgl.add(menuNode9);
+        JSONObject menuNode0 = new JSONObject();
+        menuNode0.put("nodeId", "JSPZGL");
+        menuNode0.put("name", "计算配置管理");
+        menuNode0.put("nodeType", "GROUP");
+        menuNode0.put("children", menuNodeListJspzgl);
+
+        JSONArray menuRootList = new JSONArray();
+        menuRootList.add(menuNode0);
+        JSONObject result = new JSONObject();
+        result.put("data", menuRootList);
+
+        return result.toJSONString();
     }
 
 }
